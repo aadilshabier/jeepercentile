120bc11d5d8651f4f2b2e145327f1c9cf68ee6cf
Mohammad Aadil Shabier <64919407+aadilshabier@users.noreply.github.com>
2021-03-21 00:31:52 +0530

    Moved runtime constants to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+starting_rank = 2000
+ending_rank = 4500
+no_of_plots = 5
+expected_students_lower = 550_000
+expected_students_upper = 610_000
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-starting_rank = 2000
-ending_rank = 4500
-no_of_plots = 5
-expected_students_lower = 550_000
-expected_students_upper = 610_000
-
 
 def percentile(x, rank):
     return (x-rank)/x*100
